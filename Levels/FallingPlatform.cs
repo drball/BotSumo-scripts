18e08d7da052e4a9339371e891a1d925499f0a2f
David <david@burnthebook.co.uk>
2016-12-30 16:50:08 +0000

    Code for falling platforms in C#

---
+++ Levels/FallingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+
+	public bool falling = false;
+	private float fallSpeed = 6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(falling == true){
+			Debug.Log("falling - y = "+transform.localPosition.z);
+			transform.localPosition.z -= fallSpeed;
+		}
+	}
+}
---
+++ Levels/FallingPlatformController.cs
@@ -0,0 +1,44 @@
+﻿//--for the level where the platforms all fall after a few seconds
+using UnityEngine;
+using System.Collections;
+
+public class FallingPlatformController : MonoBehaviour {
+
+	private int maxNum = 246;
+	private GameObject CurrentPlatform;
+	private Renderer rend;
+	public Color highlightColor;
+	private Color initialColor;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("SelectFallingPlatform", 1, 1.5f);
+	}
+	
+	void SelectFallingPlatform(){
+
+		var randomNum = Random.Range(0, maxNum);
+		var platformName = "FallingPlatform ("+randomNum.ToString()+")";
+		Debug.Log("platform to fall is "+platformName);
+
+		CurrentPlatform = transform.Find(platformName).gameObject;
+
+		if(CurrentPlatform != null){
+
+			Debug.Log("remove "+platformName);
+			rend = CurrentPlatform.GetComponent<Renderer>();
+			initialColor = rend.material.color;
+			// FallingPlatform platformScript = ;
+
+			GetComponent<FallingPlatform>().falling = true;
+
+			// CurrentPlatform.GetComponent<Rigidbody>().isKinematic = false;
+
+		}
+
+		
+	}
+
+	
+}
